dfb64be82763e6c21bfb27118140df81e627fb5b
Raphael Yoshiga <raphael.yoshiga@hotmail.com>
2020-06-18 20:15:44 +0100

    Implement concert product strategy, create default product strategy

---
+++ csharp/ProductStrategies/ConcertProductQualityUpdateStrategy.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace csharp
+{
+    public class ConcertProductQualityUpdateStrategy : IProductQualityUpdateStrategy
+    {
+        public void UpdateQuality(Item item)
+        {
+            if (item.SellIn <= 0)
+            {
+                item.Quality = 0;
+            }
+            else if (item.SellIn <= 5)
+            {
+                item.Quality += 3;
+            }
+            else if (item.SellIn <= 10)
+            {
+                item.Quality += 2;
+            }
+            else
+            {
+                item.Quality++;
+            }
+
+
+            if (item.Quality > 50)
+            {
+                item.Quality = 50;
+            }
+
+            item.SellIn--;
+        }
+    }
+}
--- csharp/ProductStrategyFactory.cs
+++ csharp/ProductStrategyFactory.cs
@@ -5,16 +5,32 @@
         public IProductQualityUpdateStrategy Instantiate(Item item)
         {
             if (item.Name == "Sulfuras, Hand of Ragnaros")
-            {
                 return new SulfurasQualityUpdateStrategy();
-            }
 
             if (item.Name == "Aged Brie")
-            {
                 return new AgedBrieQualityUpdateStrategy();
-            }
 
-            return null;
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                return new ConcertProductQualityUpdateStrategy();
+
+            return new DefaultStrategy();
+        }
+    }
+
+    public class DefaultStrategy : IProductQualityUpdateStrategy
+    {
+        public void UpdateQuality(Item item)
+        {
+            if (item.Quality > 0)
+                item.Quality--;
+
+            item.SellIn = item.SellIn - 1;
+
+            if (item.SellIn >= 0) 
+                return;
+
+            if (item.Quality > 0)
+                item.Quality --;
         }
     }
 }
