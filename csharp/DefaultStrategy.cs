94acb270d6ea3c3652b1ad88d7bdb609e12723ac
Raphael Yoshiga <raphael.yoshiga@hotmail.com>
2020-06-18 20:16:32 +0100

    Default strategy, adjust namespaces

---
+++ csharp/DefaultStrategy.cs
@@ -0,0 +1,19 @@
+﻿namespace csharp
+{
+    public class DefaultStrategy : IProductQualityUpdateStrategy
+    {
+        public void UpdateQuality(Item item)
+        {
+            if (item.Quality > 0)
+                item.Quality--;
+
+            item.SellIn = item.SellIn - 1;
+
+            if (item.SellIn >= 0) 
+                return;
+
+            if (item.Quality > 0)
+                item.Quality --;
+        }
+    }
+}
--- csharp/ProductStrategies/ProductStrategyFactory.cs
+++ csharp/ProductStrategies/ProductStrategyFactory.cs
@@ -16,21 +16,4 @@
             return new DefaultStrategy();
         }
     }
-
-    public class DefaultStrategy : IProductQualityUpdateStrategy
-    {
-        public void UpdateQuality(Item item)
-        {
-            if (item.Quality > 0)
-                item.Quality--;
-
-            item.SellIn = item.SellIn - 1;
-
-            if (item.SellIn >= 0) 
-                return;
-
-            if (item.Quality > 0)
-                item.Quality --;
-        }
-    }
 }
