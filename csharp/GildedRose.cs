6cefdea305e2de46d476b6ead6ff27fcc9c92e32
Raphael Yoshiga <raphael.yoshiga@hotmail.com>
2020-06-18 21:44:02 +0100

    Small move of the factory to constructor level

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -6,9 +6,12 @@ namespace csharp
     public class GildedRose
     {
         IList<Item> Items;
+        private ProductStrategyFactory _productStrategyFactory;
+
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
+            _productStrategyFactory = new ProductStrategyFactory();
         }
 
         public void UpdateQuality()
@@ -21,7 +24,7 @@ namespace csharp
 
         private void UpdateQuality(Item item)
         {
-            var strategy = new ProductStrategyFactory().Instantiate(item);
+            var strategy = _productStrategyFactory.Instantiate(item);
             strategy.UpdateQuality(item);
         }
     }
